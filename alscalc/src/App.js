6f26f8be3e4a4749927f498ca220eff8e6b25b6b
Savith Jayasekera <scjayase@ucalgary.ca>
2019-05-06 04:05:36 -0600

    added basic design. no styling

--- alscalc/src/App.js
+++ alscalc/src/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
-
-import Physical from './Components/Physical/Physical'
+import {Toggle} from "react-toggle-component"
 
 class App extends Component {
   state = {
     
-      brainstem: {umn: false, lmn: false, fibs: false, fasics: false, chronicDenerv: false},
-      cervical: {umn: false, lmn: false, fibs: false, fasics: false, chronicDenerv: false},
-      thoracic: {umn: false, lmn: false, fibs: false, fasics: false, chronicDenerv: false},
-      lumbosacral: {umn: false, lmn: false, fibs: false, fasics: false, chronicDenerv: false},
+      regions: [
+      {id: "brainstem", umn: false, lmn: false, fibs: false, fasics: false, chronicDenerv: false},
+      {id: "cervical", umn: false, lmn: false, fibs: false, fasics: false, chronicDenerv: false},
+      {id: "thoracic", umn: false, lmn: false, fibs: false, fasics: false, chronicDenerv: false},
+      {id: "lumbosacral", umn: false, lmn: false, fibs: false, fasics: false, chronicDenerv: false}],
 
       gene:false,
       tilt:false,
@@ -20,9 +20,49 @@ class App extends Component {
 
   };
 
+  changedHandler = (event, id, finding) => {
+    const regionIndex = this.state.regions.findIndex(p => {
+      return p.id === id;
+    });
+
+    const region = {
+      ...this.state.regions[regionIndex]
+    };
+
+    switch(finding){
+      case 0:
+        region.umn = event.target.checked;
+        break;
+      case 1:
+        region.lmn = event.target.checked;
+        break;
+      case 2:
+        region.fibs = event.target.checked;
+        break;
+      case 3:
+        region.fasics = event.target.checked;
+        break;
+      case 4:
+        region.chronicDenerv = event.target.checked;
+    };
+
+    const regions = [...this.state.regions];
+
+    regions[regionIndex] = region;
+
+    this.setState( {regions:regions} )
+
+
+  };
+
   togglePhysicalHandler = () => {
-    const doesShow = this.state.showPhysical;
+    const doesShow = this.state.showPhysical
     this.setState({showPhysical: !doesShow});
+  }
+
+  toggleLabHandler = () => {
+    const doesShow = this.state.showLab
+    this.setState({showLab: !doesShow});
   }
 
   render(){
@@ -32,18 +72,87 @@ class App extends Component {
     if (this.state.showPhysical) {
        physicalComponent = (
           <div>
-            <Physical/>
+            {this.state.regions.map((region)=>{
+            return (
+              <div key={region.id}>
+                {region.id}
+            <Toggle
+              name={region.id + "umn"} 
+              onChange={(event) => this.changedHandler(event, region.id, 0)}
+              checked={region.umn}/>
+
+              <Toggle
+              name={region.id + "lmn"} 
+              onChange={(event) => this.changedHandler(event, region.id, 1)}
+              checked={region.lmn}/>
+
+              <hr />
+
+              </div>
+              
+              )
+          })}
           </div>
         )
+    };
+
+    let labComponent = null;
+
+    if (this.state.showLab){
+
+      labComponent = (
+
+        <div>
+            {this.state.regions.map((region)=>{
+            return (
+              <div key={region.id}>
+                {region.id}
+            <Toggle
+              name={region.id + "fibs"} 
+              onChange={(event) => this.changedHandler(event, region.id, 2)}
+              checked={region.fibs}/>
+
+              <Toggle
+              name={region.id + "fasics"} 
+              onChange={(event) => this.changedHandler(event, region.id, 3)}
+              checked={region.fasics}/>
+
+              <Toggle
+              name={region.id + "chronic"} 
+              onChange={(event) => this.changedHandler(event, region.id, 4)}
+              checked={region.chronicDenerv}/>    
+
+              <hr />
+
+              </div>
+              
+              )
+          })}
+          </div>
+      )
+
+
     };
 
     return (
       <div>
         <h1>ALS Calculator</h1>
         <button 
-          onClick={this.togglePhysicalHandler}>Physical
+          onClick={this.togglePhysicalHandler}>
+          Physical
         </button>
         {physicalComponent}
+
+        <button 
+          onClick={this.toggleLabHandler}>
+          Lab
+        </button>
+        {labComponent}
+
+        <button
+          onClick={this.calculateHandler}>
+          Calculate
+        </button>
       </div>
     );
 
