48f5a0782af6c0871142178192631d21ecceb221
Savith Jayasekera <scjayase@ucalgary.ca>
2019-07-23 00:25:30 -0600

    update

--- alscalc/src/Model/ElEscorial.js
+++ alscalc/src/Model/ElEscorial.js
@@ -129,7 +129,6 @@ class ElEscorial {
     return false;
   }
 
-  //returns true if a finding is present according to the El Escorial criteria
   isFindingPresent(finding, region) {
     return this.isPhysicalFindingPresent(finding, region);
   }
